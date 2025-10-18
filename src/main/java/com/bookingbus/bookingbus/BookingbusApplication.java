06df7672b9d7d59616776ade3fca6be127601d7f
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-10-18 16:12:45 +0530

    init commit

---
+++ src/main/java/com/bookingbus/bookingbus/BookingbusApplication.java
@@ -0,0 +1,13 @@
+package com.bookingbus.bookingbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookingbus/bookingbus/BookingbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingbus.bookingbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
