fd6c95a3f057bab1565801f17ef220560d5eb99b
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-10-23 01:35:32 +0530

    Add war configutaion

--- src/main/java/com/bookingbus/bookingbus/BookingbusApplication.java
+++ src/main/java/com/bookingbus/bookingbus/BookingbusApplication.java
@@ -2,12 +2,18 @@ package com.bookingbus.bookingbus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookingbusApplication {
+public class BookingbusApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BookingbusApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingbusApplication.class, args);
 	}
-
 }
